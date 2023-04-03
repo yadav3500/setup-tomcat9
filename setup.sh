8057e1f2c90b81e59dafb6bf71ee9af891147429
yadav3500 <122116050+yadav3500@users.noreply.github.com>
2023-04-03 18:07:04 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install openjdk-11-jdk
+sudo apt-cache search tomcat
+sudo apt install tomcat9 tomcat9-admin -y
+ss -ltn
+sudo ufw allow from any to any port 8080 proto tcp
